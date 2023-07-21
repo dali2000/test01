df43b95eddc7e613464a70e91ce6c91e3412ac02
dali2000 <medalikebair@gmail.com>
2023-07-21 15:39:19 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+//rl importation mta3 express
+const express = require('express');
+
+
+const data = {
+    name: 'dali',
+    forname:'kbaier',
+    age: 23,
+}
+
+//express fonction
+const app = express();
+
+//get function
+app.get('/user', function(req, res){
+    res.send(200,data);
+})
+
+//ma3naha bich iwali yaamel connect 3al port 8080 el serveur lien mta3ou "localhost:8080"
+app.listen(8080, function () {
+    console.log('listening on 3006   port');
+}); 
